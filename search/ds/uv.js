df6a148fb0e9382b6e10008088b46f5d099e2618
slitherdmon <105375648+slitherdmon@users.noreply.github.com>
2025-04-25 18:24:15 +0000

    Delete search/ds directory

--- search/ds/uv.js
+++
@@ -1,8 +0,0 @@
-importScripts('/helper/uv/uv.bundle.js');
-importScripts('/helper/uv/uv.config.js');
-importScripts('/helper/uv/uv.sw.js');
-importScripts('https://arc.io/arc-sw-core.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
