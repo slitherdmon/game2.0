7c225610630fcef9ec6c5e92c61f3cda236b19bd
slitherdmon <105375648+slitherdmon@users.noreply.github.com>
2025-04-25 13:25:18 -0500

    Add files via upload

---
+++ search/ds/uv-sw.js
@@ -0,0 +1,5 @@
+importScripts("/search/ds/uv/uv.sw.js");
+
+const sw = new UVServiceWorker();
+
+self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
---
+++ search/ds/uv.js
@@ -0,0 +1,8 @@
+importScripts('/search/ds/uv/uv.bundle.js');
+importScripts('/search/ds/uv/uv.config.js');
+importScripts('/search/ds/uv/uv.sw.js');
+importScripts('https://arc.io/arc-sw-core.js');
+
+const sw = new UVServiceWorker();
+
+self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
